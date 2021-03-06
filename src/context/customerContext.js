160b4e7c76b4f20fdc0865b6c9e9d9e90a3ad9bb
Allister <allisterrampenthal@gmail.com>
2021-03-06 14:20:55 -0600

    adding customer context file

---
+++ src/context/customerContext.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default customerContext = React.createContext({
+  salesperson: undefined,
+  customer1: undefined,
+  customer2: undefined,
+  address1: undefined,
+  address2: undefined,
+  city: undefined,
+  state: undefined,
+  zipcode: undefined,
+  phone: undefined,
+  email: undefined,
+});
