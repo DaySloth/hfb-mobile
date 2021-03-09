76c0a200a82ca2f0711eb27426c72d1bac031329
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-03-09 16:33:02 -0600

    Update Navigator.js

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/Home";
 import CustomerInfo from "../screens/CustomerInfo";
+import Details from "../screens/Details";
 import * as React from "react";
 
 const Drawer = createDrawerNavigator();
