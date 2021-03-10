b0dd73084dcaa015d680dbb874af98473d8833d7
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-03-10 17:30:05 -0600

    Update Navigator.js

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/Home";
 import CustomerInfo from "../screens/CustomerInfo";
 import Details from "../screens/Details";
+import Login from "../screens/Login";
 import * as React from "react";
 
 const Drawer = createDrawerNavigator();
@@ -11,6 +12,7 @@ export default function Navigator() {
     return (
         <NavigationContainer>
             <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen name="Login" component={Login} />
                 <Drawer.Screen name="Home" component={Home} />
                 <Drawer.Screen name="Customer Info" component={CustomerInfo} />
                 <Drawer.Screen name="Details" component={Details} />
