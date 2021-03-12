9a5f1775ac60e1060530b0d712e81b2889cd0d98
Allister <allisterrampenthal@gmail.com>
2021-03-11 19:17:58 -0600

    adding background image

--- App.js
+++ App.js
@@ -6,62 +6,63 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as LocalAuthentication from "expo-local-authentication";
 
 export default function App() {
-    useEffect(() => {
-        checkLocalUser();
-    }, []);
+  useEffect(() => {
+    checkLocalUser();
+  }, []);
 
-    const [salesPerson, setSalesPerson] = useState("");
-    const [customer, setCustomer] = useState({
-        customer1: undefined,
-        customer2: undefined,
-        address1: undefined,
-        address2: undefined,
-        city: undefined,
-        state: undefined,
-        zipcode: undefined,
-        phone: undefined,
-        email: undefined,
-    });
-    const [user, setUser] = useState({
-        isLoggedIn: false,
-        first_name: undefined,
-        last_name: undefined,
-        email: undefined,
-    });
+  const [salesPerson, setSalesPerson] = useState("");
+  const [customer, setCustomer] = useState({
+    customer1: undefined,
+    customer2: undefined,
+    address1: undefined,
+    address2: undefined,
+    city: undefined,
+    state: undefined,
+    zipcode: undefined,
+    phone: undefined,
+    email: undefined,
+  });
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    first_name: undefined,
+    last_name: undefined,
+    email: undefined,
+  });
 
-    async function checkLocalUser() {
-        let localUser = await AsyncStorage.getItem("hfbUserData");
-        if (localUser) {
-            localUser = JSON.parse(localUser);
-            const lastLogin = new Date(localUser.last_login).getTime();
-            const oneDay = new Date().getTime() + 1 * 24 * 60 * 60 * 1000;
-            if (oneDay > lastLogin) {
-                if (await LocalAuthentication.hasHardwareAsync()) {
-                    if (await LocalAuthentication.isEnrolledAsync()) {
-                        if (
-                            await LocalAuthentication.authenticateAsync({
-                                promptMessage: `Verify this is ${localUser.first_name}`,
-                            })
-                        ) {
-                            setUser({ isLoggedIn: true, ...localUser });
-                        }
-                    } else {
-                        //prompt for a login code
-                    }
-                } else {
-                    //prompt for a login code
-                }
+  async function checkLocalUser() {
+    let localUser = await AsyncStorage.getItem("hfbUserData");
+    if (localUser) {
+      localUser = JSON.parse(localUser);
+      const lastLogin = new Date(localUser.last_login).getTime();
+      const oneDay = new Date().getTime() + 1 * 24 * 60 * 60 * 1000;
+      if (oneDay > lastLogin) {
+        if (await LocalAuthentication.hasHardwareAsync()) {
+          if (await LocalAuthentication.isEnrolledAsync()) {
+            if (
+              await LocalAuthentication.authenticateAsync({
+                promptMessage: `Verify this is ${localUser.first_name}`,
+              })
+            ) {
+              setSalesPerson(`${localUser.first_name} ${localUser.last_name}`);
+              setUser({ isLoggedIn: true, ...localUser });
             }
+          } else {
+            //prompt for a login code
+          }
+        } else {
+          //prompt for a login code
         }
+      }
     }
+  }
 
-    return (
-        <CustomerContext.Provider
-            value={{ salesPerson, setSalesPerson, customer, setCustomer }}
-        >
-            <UserContext.Provider value={{ user, setUser }}>
-                <AppNavigator />
-            </UserContext.Provider>
-        </CustomerContext.Provider>
-    );
+  return (
+    <CustomerContext.Provider
+      value={{ salesPerson, setSalesPerson, customer, setCustomer }}
+    >
+      <UserContext.Provider value={{ user, setUser }}>
+        <AppNavigator />
+      </UserContext.Provider>
+    </CustomerContext.Provider>
+  );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,60 +1,71 @@
 import React, { useContext, useState } from "react";
-import {
-    SafeAreaView,
-    Text,
-    View,
-    StyleSheet,
-    Alert,
-    Keyboard,
-} from "react-native";
+import { View, StyleSheet, ImageBackground } from "react-native";
 import { Button, Input, Image, Header } from "react-native-elements";
 import Logo from "../../assets/mobile-logo-horz-transparent.png";
 import CustomerContext from "../context/customerContext";
+import Background from "../../assets/tile-background.jpg";
 
 export default function HomeScreen({ navigation }) {
-    const { salesPerson, setSalesPerson } = useContext(CustomerContext);
-    const [error, setError] = useState("");
+  const { salesPerson, setSalesPerson, setCustomer } = useContext(
+    CustomerContext
+  );
+  const [error, setError] = useState("");
 
-    return (
-        <View style={styles.container}>
-            <Header
-                leftComponent={{
-                    icon: "menu",
-                    color: "#fff",
-                    onPress: () => navigation.openDrawer(),
-                }}
-                centerComponent={{
-                    text: "Home Forever Baths Mobile",
-                    style: { color: "#fff", fontSize: 18 },
-                }}
-                rightComponent={{
-                    icon: "home",
-                    color: "#fff",
-                    onPress: () => navigation.navigate("Home"),
-                }}
-                backgroundColor="black"
-                statusBarProps={{ barStyle: "light-content" }}
-            />
-            <Image
-                source={Logo}
-                style={{ width: 500, height: 300, marginTop: 100 }}
-                resizeMode="contain"
-            />
+  return (
+    <View style={styles.container}>
+      <Header
+        leftComponent={{
+          icon: "menu",
+          color: "#fff",
+          onPress: () => navigation.openDrawer(),
+        }}
+        centerComponent={{
+          text: "Home Forever Baths Mobile",
+          style: { color: "#fff", fontSize: 18 },
+        }}
+        rightComponent={{
+          icon: "home",
+          color: "#fff",
+          onPress: () => navigation.navigate("Home"),
+        }}
+        backgroundColor="black"
+        statusBarProps={{ barStyle: "light-content" }}
+      />
+      <ImageBackground style={styles.backgroundImage} source={Background}>
+        <Image
+          source={Logo}
+          style={{ width: 500, height: 300, marginTop: 100 }}
+          resizeMode="contain"
+        />
 
-            <Button
-                title="New Order"
-                onPress={() => {
-                    navigation.navigate("Customer Info");
-                }}
-            />
-        </View>
-    );
+        <Button
+          title="New Order"
+          style={{ width: 200, alignSelf: "center" }}
+          onPress={() => {
+            setCustomer({
+              customer1: undefined,
+              customer2: undefined,
+              address1: undefined,
+              address2: undefined,
+              city: undefined,
+              state: undefined,
+              zipcode: undefined,
+              phone: undefined,
+              email: undefined,
+            });
+            navigation.navigate("Customer Info");
+          }}
+        />
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  backgroundImage: { flex: 1 },
 });
