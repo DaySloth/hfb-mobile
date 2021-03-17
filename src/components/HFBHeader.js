639849176c984cba23fb9268bfb0347d20a79799
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-03-17 13:22:17 -0500

    Update HFBHeader.js

--- src/components/HFBHeader.js
+++ src/components/HFBHeader.js
@@ -1,36 +1,64 @@
-import React from "react";
-import { Image, Header } from "react-native-elements";
+import React, { useState } from "react";
+import { Image, Header, Overlay } from "react-native-elements";
+import { StyleSheet, Text, View } from "react-native";
 import Logo from "../../assets/Kohler-hfb-divider.png";
 
 export default function HFBHeader({ navigation }) {
-  return (
-    <Header
-      leftComponent={{
-        icon: "menu",
-        color: "#fff",
-        onPress: () => navigation.openDrawer(),
-        style: { textAlignVertical: "center" },
-        size: 30,
-      }}
-      centerComponent={
-        <Image
-          source={Logo}
-          style={{
-            width: 200,
-            height: 40,
-            resizeMode: "contain",
-          }}
-        />
-      }
-      rightComponent={{
-        icon: "home",
-        color: "#fff",
-        onPress: () => navigation.navigate("Home"),
-        style: { textAlignVertical: "center" },
-        size: 30,
-      }}
-      backgroundColor="#7a7a7a"
-      statusBarProps={{ barStyle: "light-content" }}
-    />
-  );
+    const [visible, setVisible] = useState(false);
+
+    const toggleOverlay = () => {
+        setVisible(!visible);
+    };
+    return (
+        <>
+            <Header
+                leftComponent={{
+                    icon: "menu",
+                    color: "#fff",
+                    onPress: () => navigation.openDrawer(),
+                    style: { textAlignVertical: "center" },
+                    size: 30,
+                }}
+                centerComponent={
+                    <Image
+                        source={Logo}
+                        style={{
+                            width: 200,
+                            height: 40,
+                            resizeMode: "contain",
+                        }}
+                        onPress={() => navigation.navigate("Home")}
+                    />
+                }
+                rightComponent={{
+                    icon: "receipt",
+                    color: "#fff",
+                    onPress: toggleOverlay,
+                    style: { textAlignVertical: "center" },
+                    size: 30,
+                }}
+                backgroundColor="#7a7a7a"
+                statusBarProps={{ barStyle: "light-content" }}
+            />
+            <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+                <View style={styles.overlay}>
+                    <Text
+                        style={{
+                            fontSize: 20,
+                        }}
+                    >
+                        Current Install Details
+                    </Text>
+                </View>
+            </Overlay>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    overlay: {
+        width: "100%",
+        height: "70%",
+        padding: 10,
+    },
+});
