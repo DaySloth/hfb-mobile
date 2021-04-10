15562e747a57aa3486aee1ce677696cf6e58ba49
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-04-09 19:01:06 -0500

    Update Parts.js

--- src/screens/Parts.js
+++ src/screens/Parts.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
 	View,
 	StyleSheet,
@@ -7,10 +7,22 @@ import {
 	Platform,
 	KeyboardAvoidingView,
 	ScrollView,
+	TouchableOpacity,
+	Text,
 } from "react-native";
-import { Button, Input, Image, Header, Icon } from "react-native-elements";
+import {
+	Button,
+	Input,
+	Image,
+	Header,
+	Icon,
+	Card,
+	ListItem,
+} from "react-native-elements";
 import InstallContext from "../context/installContext";
 import HFBHeader from "../components/HFBHeader";
+import ShowerOptions from "../components/parts/shower";
+import WalkInBathOptions from "../components/parts/walkinbath";
 
 export default function PartsScreen({ navigation }) {
 	const { projectType } = useContext(InstallContext);
@@ -19,55 +31,12 @@ export default function PartsScreen({ navigation }) {
 	return (
 		<View style={styles.container}>
 			<HFBHeader navigation={navigation} />
+			{projectType === "Shower" && <ShowerOptions />}
+			{projectType === "Digital Shower" && <ShowerOptions />}
 
-			<View style={styles.statusView}>
-				<Button
-					title="Base"
-					type="clear"
-					icon={<Icon name="check-circle-outline" size={30} color="green" />}
-					titleStyle={{ color: "black" }}
-					onPress={() => {}}
-				/>
-				<Button
-					title="Drain"
-					type="clear"
-					icon={<Icon name="highlight-off" size={30} color="gray" />}
-					titleStyle={{ color: "black" }}
-					onPress={() => {}}
-				/>
-				<Button
-					title="Wall System"
-					type="clear"
-					icon={<Icon name="highlight-off" size={30} color="gray" />}
-					titleStyle={{ color: "black" }}
-					onPress={() => {}}
-				/>
-				<Button
-					title="Enclosure"
-					type="clear"
-					icon={<Icon name="highlight-off" size={30} color="gray" />}
-					titleStyle={{ color: "black" }}
-					onPress={() => {}}
-				/>
-				<Button
-					title="Faucet"
-					type="clear"
-					icon={<Icon name="highlight-off" size={30} color="gray" />}
-					titleStyle={{ color: "black" }}
-					onPress={() => {}}
-				/>
-			</View>
-			<ScrollView>
-				<KeyboardAvoidingView
-					style={{
-						flex: 1,
-						marginTop: 30,
-						flexDirection: "column",
-						justifyContent: "center",
-					}}
-					behavior="padding"
-				></KeyboardAvoidingView>
-			</ScrollView>
+			{projectType === "Digital Shower(Free Upgrade)" && <ShowerOptions />}
+
+			{projectType === "Walk In Bath" && <WalkInBathOptions />}
 		</View>
 	);
 }
@@ -86,5 +55,16 @@ const styles = StyleSheet.create({
 		borderBottomColor: "gray",
 		borderBottomWidth: 2,
 		padding: 10,
+	},
+	cardContainer: {
+		flex: 1,
+		width: Dimensions.get("window").width,
+		alignItems: "center",
+	},
+	card: {
+		width: 350,
+		height: 350,
+		borderRadius: 30,
+		alignSelf: "center",
 	},
 });
