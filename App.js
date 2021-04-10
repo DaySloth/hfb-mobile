982f08e16133601f9fd3c196a95b7fd1c8c94e66
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-04-09 19:00:48 -0500

    Update App.js

--- App.js
+++ App.js
@@ -8,169 +8,168 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as LocalAuthentication from "expo-local-authentication";
 
 export default function App() {
-    useEffect(() => {
-        checkLocalUser();
-    }, []);
+	useEffect(() => {
+		checkLocalUser();
+	}, []);
 
-    function clearEstimate() {
-        setCustomer({
-            customer1: undefined,
-            customer2: undefined,
-            address1: undefined,
-            address2: undefined,
-            city: undefined,
-            state: undefined,
-            zipcode: undefined,
-            phone: undefined,
-            email: undefined,
-        });
+	function clearEstimate() {
+		setCustomer({
+			customer1: undefined,
+			customer2: undefined,
+			address1: undefined,
+			address2: undefined,
+			city: undefined,
+			state: undefined,
+			zipcode: undefined,
+			phone: undefined,
+			email: undefined,
+		});
 
-        setProjectType("");
-        setInstallStory("");
-        setTypeOfHome("");
-        setWorkspaceLocation("");
-        setBathroomLocation("");
-        setOnlyBathroom("");
-        setWaterShutOff("");
-        setHasBasement(null);
-        setBasementCondition("");
-        setIsPermitRequired("");
-        setHoa(undefined);
-        setHoaDetails("");
+		setProjectType("");
+		setInstallStory("");
+		setTypeOfHome("");
+		setWorkspaceLocation("");
+		setBathroomLocation("");
+		setOnlyBathroom("");
+		setWaterShutOff("");
+		setHasBasement(null);
+		setBasementCondition("");
+		setIsPermitRequired("");
+		setHoa(undefined);
+		setHoaDetails("");
 
-        setLength("");
-        setWidth("");
-        setHeight("");
-        setAvailableLength(0);
-        setAvailableWidth(0);
-        setAvailableHeight(0);
-    }
+		setLength("");
+		setWidth("");
+		setHeight("");
+		setAvailableLength(0);
+		setAvailableWidth(0);
+		setAvailableHeight(0);
+	}
 
-    const [salesPerson, setSalesPerson] = useState("");
-    const [customer, setCustomer] = useState({
-        customer1: undefined,
-        customer2: undefined,
-        address1: undefined,
-        address2: undefined,
-        city: undefined,
-        state: undefined,
-        zipcode: undefined,
-        phone: undefined,
-        email: undefined,
-    });
-    const [user, setUser] = useState({
-        isLoggedIn: false,
-        first_name: undefined,
-        last_name: undefined,
-        email: undefined,
-    });
+	const [salesPerson, setSalesPerson] = useState("");
+	const [customer, setCustomer] = useState({
+		customer1: undefined,
+		customer2: undefined,
+		address1: undefined,
+		address2: undefined,
+		city: undefined,
+		state: undefined,
+		zipcode: undefined,
+		phone: undefined,
+		email: undefined,
+	});
+	const [user, setUser] = useState({
+		isLoggedIn: false,
+		first_name: undefined,
+		last_name: undefined,
+		email: undefined,
+	});
 
-    const [projectType, setProjectType] = useState("");
-    const [installStory, setInstallStory] = useState("");
-    const [typeOfHome, setTypeOfHome] = useState("");
-    const [workspaceLocation, setWorkspaceLocation] = useState("");
-    const [bathroomLocation, setBathroomLocation] = useState("");
-    const [onlyBathroom, setOnlyBathroom] = useState("");
-    const [waterShutOff, setWaterShutOff] = useState("");
-    const [hasBasement, setHasBasement] = useState(null);
-    const [basementCondition, setBasementCondition] = useState("");
-    const [isPermitRequired, setIsPermitRequired] = useState("");
-    const [hoa, setHoa] = useState("");
-    const [hoaDetails, setHoaDetails] = useState("");
+	const [projectType, setProjectType] = useState("");
+	const [installStory, setInstallStory] = useState("");
+	const [typeOfHome, setTypeOfHome] = useState("");
+	const [workspaceLocation, setWorkspaceLocation] = useState("");
+	const [bathroomLocation, setBathroomLocation] = useState("");
+	const [onlyBathroom, setOnlyBathroom] = useState("");
+	const [waterShutOff, setWaterShutOff] = useState("");
+	const [hasBasement, setHasBasement] = useState(null);
+	const [basementCondition, setBasementCondition] = useState("");
+	const [isPermitRequired, setIsPermitRequired] = useState("");
+	const [hoa, setHoa] = useState("");
+	const [hoaDetails, setHoaDetails] = useState("");
 
-    const [length, setLength] = useState("");
-    const [width, setWidth] = useState("");
-    const [height, setHeight] = useState("");
-    const [availableLength, setAvailableLength] = useState(0);
-    const [availableWidth, setAvailableWidth] = useState(0);
-    const [availableHeight, setAvailableHeight] = useState(0);
+	const [length, setLength] = useState("");
+	const [width, setWidth] = useState("");
+	const [height, setHeight] = useState("");
+	const [availableLength, setAvailableLength] = useState(0);
+	const [availableWidth, setAvailableWidth] = useState(0);
+	const [availableHeight, setAvailableHeight] = useState(0);
 
-    async function checkLocalUser() {
-        let localUser = await AsyncStorage.getItem("hfbUserData");
-        if (localUser) {
-            localUser = JSON.parse(localUser);
-            if (await LocalAuthentication.hasHardwareAsync()) {
-                if (await LocalAuthentication.isEnrolledAsync()) {
-                    if (
-                        await LocalAuthentication.authenticateAsync({
-                            promptMessage: `Verify this is ${localUser.first_name}`,
-                        })
-                    ) {
-                        setSalesPerson(
-                            `${localUser.first_name} ${localUser.last_name}`
-                        );
-                        setUser({ isLoggedIn: true, ...localUser });
-                    }
-                } else {
-                    //prompt for a login code
-                }
-            } else {
-                //prompt for a login code
-            }
-        }
-    }
+	async function checkLocalUser() {
+		let localUser = await AsyncStorage.getItem("hfbUserData");
+		if (localUser) {
+			localUser = JSON.parse(localUser);
+			if (await LocalAuthentication.hasHardwareAsync()) {
+				if (await LocalAuthentication.isEnrolledAsync()) {
+					const verified = await LocalAuthentication.authenticateAsync({
+						promptMessage: `Verify this is ${localUser.first_name}`,
+					});
+					if (verified.success) {
+						setSalesPerson(`${localUser.first_name} ${localUser.last_name}`);
+						setUser({ isLoggedIn: true, ...localUser });
+					} else {
+						setUser({ isLoggedIn: false });
+					}
+				} else {
+					setUser({ isLoggedIn: false });
+				}
+			} else {
+				setUser({ isLoggedIn: false });
+			}
+		}
+	}
 
-    return (
-        <CustomerContext.Provider
-            value={{
-                salesPerson,
-                setSalesPerson,
-                customer,
-                setCustomer,
-                clearEstimate,
-            }}
-        >
-            <UserContext.Provider value={{ user, setUser, clearEstimate }}>
-                <InstallContext.Provider
-                    value={{
-                        projectType,
-                        setProjectType,
-                        installStory,
-                        setInstallStory,
-                        typeOfHome,
-                        setTypeOfHome,
-                        workspaceLocation,
-                        setWorkspaceLocation,
-                        bathroomLocation,
-                        setBathroomLocation,
-                        onlyBathroom,
-                        setOnlyBathroom,
-                        waterShutOff,
-                        setWaterShutOff,
-                        hasBasement,
-                        setHasBasement,
-                        basementCondition,
-                        setBasementCondition,
-                        isPermitRequired,
-                        setIsPermitRequired,
-                        hoa,
-                        setHoa,
-                        hoaDetails,
-                        setHoaDetails,
-                        clearEstimate,
-                    }}
-                >
-                    <MeasurementContext.Provider
-                        value={{
-                            length,
-                            setLength,
-                            width,
-                            setWidth,
-                            height,
-                            setHeight,
-                            availableLength,
-                            setAvailableLength,
-                            availableWidth,
-                            setAvailableWidth,
-                            availableHeight,
-                            setAvailableHeight,
-                            clearEstimate,
-                        }}
-                    >
-                        <AppNavigator />
-                    </MeasurementContext.Provider>
-                </InstallContext.Provider>
-            </UserContext.Provider>
-        </CustomerContext.Provider>
-    );
+	return (
+		<CustomerContext.Provider
+			value={{
+				salesPerson,
+				setSalesPerson,
+				customer,
+				setCustomer,
+				clearEstimate,
+			}}
+		>
+			<UserContext.Provider value={{ user, setUser, clearEstimate }}>
+				<InstallContext.Provider
+					value={{
+						projectType,
+						setProjectType,
+						installStory,
+						setInstallStory,
+						typeOfHome,
+						setTypeOfHome,
+						workspaceLocation,
+						setWorkspaceLocation,
+						bathroomLocation,
+						setBathroomLocation,
+						onlyBathroom,
+						setOnlyBathroom,
+						waterShutOff,
+						setWaterShutOff,
+						hasBasement,
+						setHasBasement,
+						basementCondition,
+						setBasementCondition,
+						isPermitRequired,
+						setIsPermitRequired,
+						hoa,
+						setHoa,
+						hoaDetails,
+						setHoaDetails,
+						clearEstimate,
+					}}
+				>
+					<MeasurementContext.Provider
+						value={{
+							length,
+							setLength,
+							width,
+							setWidth,
+							height,
+							setHeight,
+							availableLength,
+							setAvailableLength,
+							availableWidth,
+							setAvailableWidth,
+							availableHeight,
+							setAvailableHeight,
+							clearEstimate,
+						}}
+					>
+						<AppNavigator />
+					</MeasurementContext.Provider>
+				</InstallContext.Provider>
+			</UserContext.Provider>
+		</CustomerContext.Provider>
+	);
 }
